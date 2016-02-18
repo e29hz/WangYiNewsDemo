4eff61fc78b3ea5aec928e206bc31a5b95274b63
ehongzhen <ehongzhen@qq.com>
2016-02-18 17:18:41 +0800

    Initial Commit

---
+++ WangYiNewsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WangYiNewsDemo
+//
+//  Created by 鄂鸿桢 on 16/2/18.
+//  Copyright © 2016年 ehongzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WangYiNewsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WangYiNewsDemo
+//
+//  Created by 鄂鸿桢 on 16/2/18.
+//  Copyright © 2016年 ehongzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
